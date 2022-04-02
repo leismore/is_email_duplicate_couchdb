bc6f38e808df6c1f5ab7aeb23dd059cd18747c6b
Kyle Chine <kyle.chine@leismore.co>
2022-04-02 18:55:06 +1100

    bugfix: email lowercase

--- user/is_email_duplicate/get_userID_email.js
+++ user/is_email_duplicate/get_userID_email.js
@@ -6,6 +6,6 @@ function(doc)
 {
     if (doc.userID !== undefined && (doc.email !== undefined && 'address' in doc.email) )
     {
-        emit(doc.email.address, doc.userID);
+        emit(doc.email.address.toLowerCase(), doc.userID);
     }
 }
